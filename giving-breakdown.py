ee7c64b9ae53612e4aadf570cc27c40ba04e6d6d
Jacob Vardilos <jvardilos@Jacobs-Mac-mini.localdomain>
2024-09-12 16:42:42 -0700

    add dollar signs and take out formatting code that will not work

--- giving-breakdown.py
+++ giving-breakdown.py
@@ -1,42 +1,56 @@
 import pandas as pd
 from enum import Enum
 
-input_file = "sample.csv"
+input_file = "pledge_and_giving_detail.csv"
 
 
 class Column(Enum):
-    NAME = "Name"
-    SPOUSE = "Spouse"
-    AND_SPOUSE = "Spouse2"
-    PLEDGED = "Pledged"
-    GIVEN = "Given"
+    NAME = "Name(s)"
+    # SPOUSE = "Spouse"
+    # AND_SPOUSE = "Spouse2"
+    PLEDGED = "Total Pledged"
+    GIVEN = "Total Given (all-time)"
     EMAIL = "Email"
 
 
+# TODO: split names in (<last name>, <name1> & <name2>), (<last name1>, <name1>, <last name2>, <name2>)
+# and (<last name>, <name>)
 def format_couples(df):
-    df[[Column.NAME.value, Column.SPOUSE.value]] = df[Column.NAME.value].str.extract(
-        r"(\w+)(?:\s*&\s*(\w+))?", expand=True
-    )
-    df[Column.AND_SPOUSE.value] = df[Column.SPOUSE.value].apply(
-        lambda x: f"and {x}" if pd.notna(x) else ""
-    )
+    # df[[Column.NAME.value, Column.SPOUSE.value]] = df[Column.NAME.value].str.extract(
+    #     r"(\w+)(?:\s*&\s*(\w+))?", expand=True
+    # )
+    # df[Column.AND_SPOUSE.value] = df[Column.SPOUSE.value].apply(
+    #     lambda x: f"and {x}" if pd.notna(x) else ""
+    # )
 
     return df
 
 
 def filter_pledgers_and_givers(df):
-    pledged_givers = df[df[Column.PLEDGED.value] > 0]
-    givers = df[(df[Column.PLEDGED.value] == 0) & (df[Column.GIVEN.value] > 0)]
+    pledged_givers = df[df[Column.PLEDGED.value] > 0].copy()
+    givers = df[(df[Column.PLEDGED.value] == 0) & (df[Column.GIVEN.value] > 0)].copy()
 
-    return pledged_givers, givers
+    dollar_pledged_givers = convert_to_dollar(pledged_givers)
+    dollar_givers = convert_to_dollar(givers)
+
+    return dollar_pledged_givers, dollar_givers
+
+
+def convert_to_dollar(df):
+    df[Column.PLEDGED.value] = df[Column.PLEDGED.value].apply(lambda x: f"${x}")
+    df[Column.GIVEN.value] = df[Column.GIVEN.value].apply(lambda x: f"${x}")
+
+    return df
 
 
 def create_csv(title, df):
+    # Name(s),First Name,First Name (Secondary),First Name (and prefix),Email,Total Pledged,Total Given (all-time)
+
     try:
         cols = [
             Column.NAME.value,
-            Column.SPOUSE.value,
-            Column.AND_SPOUSE.value,
+            # Column.SPOUSE.value,
+            # Column.AND_SPOUSE.value,
             Column.PLEDGED.value,
             Column.GIVEN.value,
             Column.EMAIL.value,
@@ -51,8 +65,8 @@ def main():
         df = pd.read_csv(input_file)
         formatted = format_couples(df)
         pledged_givers, givers = filter_pledgers_and_givers(formatted)
-        create_csv("pledgedgivers.csv", pledged_givers)
-        create_csv("givers.csv", givers)
+        create_csv("FTO-pledge-givers.csv", pledged_givers)
+        create_csv("FTO-no-pledge-givers.csv", givers)
     except FileNotFoundError:
         print("Error: {} file not found.".format(input_file))
     except pd.errors.EmptyDataError:
