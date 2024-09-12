92c9981f4f25f82fd3ea239476f37df4affbafaa
Jacob Vardilos <jvardilos@Jacobs-Mac-mini.localdomain>
2024-09-12 09:33:38 -0700

    add error handling and adhere to pythonic rules

--- giving-breakdown.py
+++ giving-breakdown.py
@@ -1,8 +1,10 @@
 import pandas as pd
 from enum import Enum
 
+input_file = "sample.csv"
 
-class column(Enum):
+
+class Column(Enum):
     NAME = "Name"
     SPOUSE = "Spouse"
     AND_SPOUSE = "Spouse2"
@@ -12,38 +14,51 @@ class column(Enum):
 
 
 def format_couples(df):
-    df[[column.NAME.value, column.SPOUSE.value]] = df[column.NAME.value].str.extract(
+    df[[Column.NAME.value, Column.SPOUSE.value]] = df[Column.NAME.value].str.extract(
         r"(\w+)(?:\s*&\s*(\w+))?", expand=True
     )
-    df[column.AND_SPOUSE.value] = "and " + df[column.SPOUSE.value]
+    df[Column.AND_SPOUSE.value] = df[Column.SPOUSE.value].apply(
+        lambda x: f"and {x}" if pd.notna(x) else ""
+    )
+
     return df
 
 
 def filter_pledgers_and_givers(df):
-    pledged_givers = df[df[column.PLEDGED.value] > 0]
-    givers = df[(df[column.PLEDGED.value] == 0) & (df[column.GIVEN.value] > 0)]
+    pledged_givers = df[df[Column.PLEDGED.value] > 0]
+    givers = df[(df[Column.PLEDGED.value] == 0) & (df[Column.GIVEN.value] > 0)]
+
     return pledged_givers, givers
 
 
 def create_csv(title, df):
-    cols = [
-        column.NAME.value,
-        column.SPOUSE.value,
-        column.AND_SPOUSE.value,
-        column.PLEDGED.value,
-        column.GIVEN.value,
-        column.EMAIL.value,
-    ]
-    df[cols].to_csv(title, index=False)
+    try:
+        cols = [
+            Column.NAME.value,
+            Column.SPOUSE.value,
+            Column.AND_SPOUSE.value,
+            Column.PLEDGED.value,
+            Column.GIVEN.value,
+            Column.EMAIL.value,
+        ]
+        df[cols].to_csv(title, index=False)
+    except Exception as e:
+        print(f"Failed to create CSV: {e}")
 
 
 def main():
-    df = pd.read_csv("sample.csv")
-
-    formatted = format_couples(df)
-    pledged_givers, givers = filter_pledgers_and_givers(formatted)
-    create_csv("pledgedgivers.csv", pledged_givers)
-    create_csv("givers.csv", givers)
+    try:
+        df = pd.read_csv(input_file)
+        formatted = format_couples(df)
+        pledged_givers, givers = filter_pledgers_and_givers(formatted)
+        create_csv("pledgedgivers.csv", pledged_givers)
+        create_csv("givers.csv", givers)
+    except FileNotFoundError:
+        print("Error: {} file not found.".format(input_file))
+    except pd.errors.EmptyDataError:
+        print("Error: No data in the file.")
+    except Exception as e:
+        print(f"An error occurred: {e}")
 
 
 if __name__ == "__main__":
