b788afe14e74d5bfe3310068f28397fb8afa5aa9
Jacob Vardilos <jvardilos@Jacobs-Mac-mini.localdomain>
2025-02-12 16:34:35 -0800

    remove non-members, format correctly, and include "varies"

--- split.py
+++ split.py
@@ -1,7 +1,12 @@
 import pandas as pd
 from config import Column, givings_file, families_file
 from cleaning import clean_names, clean_address
-from filters import filter_pledgers, filter_pledgers_and_givers, rename_cols
+from filters import (
+    remove_non_members,
+    filter_pledgers,
+    filter_pledgers_and_givers,
+    rename_cols,
+)
 
 
 # do we want these givers to include children's giving?
@@ -19,6 +24,8 @@ def breakdowns(givings, families):
         Column.REPLACED_NAME
     ].apply(clean_names)
     contacts = clean_address(contacts)
+
+    contacts = remove_non_members(contacts)
 
     # make the splits
     pledgers, givers = filter_pledgers_and_givers(contacts)
@@ -43,19 +50,19 @@ def create_csv(title, df):
             Column.EMAIL,
             Column.ADDRESS,
         ]
-        df[cols].to_csv(title, index=False)
+        df[cols].to_csv(title, encoding="utf-8-sig", index=False)
     except Exception as e:
         print(f"Failed to create CSV: {e}")
 
 
 def main():
     try:
-        givings = pd.read_csv(givings_file)
-        families = pd.read_csv(families_file)
+        givings = pd.read_csv(givings_file, encoding="utf-8-sig")
+        families = pd.read_csv(families_file, encoding="utf-8-sig")
         fto_halfway, fto_full, givers = breakdowns(givings, families)
-        create_csv("fto_halfway.csv", fto_halfway)
-        create_csv("fto_full.csv", fto_full)
-        create_csv("givers.csv", givers)
+        create_csv("FTO_one_year_pledge.csv", fto_halfway)
+        create_csv("FTO_two_year_pledge.csv", fto_full)
+        create_csv("FTO_giving_no_pledge.csv", givers)
     except FileNotFoundError:
         print("Error: {} or {} not found.".format(givings, families))
     except pd.errors.EmptyDataError:
